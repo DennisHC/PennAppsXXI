fce736a4d6590f09282d28d82e5cce4b7883c248
melindali255 <contact.melindali@gmail.com>
2020-09-12 11:28:50 -0700

    Added basic buttons to homepage

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'users/base.html')
+    return render(request, 'users/homepage.html')
